82d2728c3f79cc3e004ae14dd28ef21ac418eb4e
faturahmanalfarisi <faturahmanalfarisi1@gmail.com>
2025-09-18 00:25:47 +0700

    Tugas Pertemuan 1

---
+++ Pertemuan1/js/lat2-eks.js
@@ -0,0 +1,3 @@
+alert("Selamat Datang di JavaScript");
+        document.write("<h1>Website JavaScript</h1>"); //Output Tercetak di Isi Web
+        console.log("Alamat Website: www.javascript.com"); //Output Tercetak di Console
